084f3bf3f993712c8fd0a47557b76dae346ac104
Josh Dorethy <78175555+jmdoor@users.noreply.github.com>
2021-10-22 13:18:18 -0500

    First Commit

---
+++ src/main/java/com/josh/AgileIntPpmToolApplication.java
@@ -0,0 +1,13 @@
+package com.josh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileIntPpmToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileIntPpmToolApplication.class, args);
+	}
+
+}
